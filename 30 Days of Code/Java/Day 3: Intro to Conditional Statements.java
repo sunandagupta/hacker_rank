f8c167f4e73e7195504e7fe708a0035bbeeff377
Sunanda Gupta <51994186+sunandagupta@users.noreply.github.com>
2021-11-04 23:51:31 +0530

    Create Day 3: Intro to Conditional Statements.java

---
+++ 30 Days of Code/Java/Day 3: Intro to Conditional Statements.java
@@ -0,0 +1,77 @@
+/*
+Objective
+In this challenge, we learn about conditional statements. Check out the Tutorial tab for learning materials and an instructional video.
+
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird; otherwise, print Not Weird.
+
+Sample Input 0
+
+3
+Sample Output 0
+
+Weird
+Sample Input 1
+
+24
+Sample Output 1
+
+Not Weird
+*/
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+    public class Solution {
+
+        public static void main(String[] args) {
+
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();            
+            String ans="";
+          
+            if(n%2==1){
+              ans = "Weird";
+            }
+            else {
+                if (n >= 2 && n <= 5) {
+                    ans = "Not Weird";
+                } 
+                
+                else if (n >= 6 && n <= 20) {
+                    ans = "Weird";
+                } 
+                
+                else if (n>20){
+                    ans = "Not Weird";
+                }
+            
+               //Complete the code
+                
+            }
+            System.out.println(ans);
+            
+        }
+    }
