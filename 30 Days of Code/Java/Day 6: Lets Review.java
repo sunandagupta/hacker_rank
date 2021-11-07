51ef611c81b517666f774c18de90825b61180656
Sunanda Gupta <51994186+sunandagupta@users.noreply.github.com>
2021-11-07 23:08:43 +0530

    Update Day 6: Lets Review.java

--- 30 Days of Code/Java/Day 6: Lets Review.java
+++ 30 Days of Code/Java/Day 6: Lets Review.java
@@ -34,4 +34,40 @@ Hce akr
 Rn ak
 */
 
-
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        int t;
+        t=scan.nextInt();
+        while(t>0){
+            
+            String str=scan.next();
+            
+            for(int i=0;i<str.length();i++){
+                
+                if(i%2==0){
+                    System.out.print(str.charAt(i));
+                }   
+            }
+          
+            System.out.print(" ");
+            
+            for(int i=0;i<str.length();i++){
+                
+                if(i%2==1){
+                    System.out.print(str.charAt(i));
+                }   
+            }  
+            
+            System.out.println();
+            t=t-1;
+        }   
+    }
+}
