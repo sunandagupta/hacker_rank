8f20b942e6e2b657bbf53e0c25e7140ff3e8adee
Sunanda Gupta <51994186+sunandagupta@users.noreply.github.com>
2021-11-08 13:30:16 +0530

    Create Weather Observation Station 3.sql

---
+++ SQL/Basic Select/Weather Observation Station 3.sql
@@ -0,0 +1,19 @@
+/*
+Query a list of CITY names from STATION with even ID numbers only. You may print the results in any order, but must exclude duplicates from your answer.
+Input Format
+The STATION table is described as follows:
+
+STATION
+Field	Type
+ID	NUMBER
+CITY	VARCHAR2(21)
+STATE	VARCHAR2(2)
+LAT_N	NUMBER
+LONG_W	NUMBER
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+
+SELECT DISTINCT CITY FROM STATION 
+WHERE ID % 2 = 0;
