9d57a33e972c50a2736616e6b2ef979687571896
Sunanda Gupta <51994186+sunandagupta@users.noreply.github.com>
2021-11-08 13:18:59 +0530

    Create Select All.sql

---
+++ SQL/Basic Select/Select All.sql
@@ -0,0 +1,16 @@
+/*
+Query all columns (attributes) for every row in the CITY table.
+Input Format
+The CITY table is described as follows:
+
+CITY
+Field	Type
+ID	NUMBER
+NAME	VARCHAR2(17)
+COUNTRYCODE	VARCHAR2(3)
+DISTRICT	VARCHAR2(20)
+POPULATION	NUMBER
+*/
+
+
+SELECT * FROM CITY;
