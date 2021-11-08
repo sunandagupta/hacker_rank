0246fc56ab0a02663a86c42cbd9b7f684c0b223f
Sunanda Gupta <51994186+sunandagupta@users.noreply.github.com>
2021-11-08 13:22:31 +0530

    Create Japanese Cities' Attribute.sql

---
+++ SQL/Basic Select/Japanese Cities' Attribute.sql
@@ -0,0 +1,17 @@
+/*
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+Input Format
+The CITY table is described as follows:
+
+CITY
+Field	Type
+ID	NUMBER
+NAME	VARCHAR2(17)
+COUNTRYCODE	VARCHAR2(3)
+DISTRICT	VARCHAR2(20)
+POPULATION	NUMBER
+*/
+
+
+SELECT * FROM CITY
+WHERE COUNTRYCODE = 'JPN';
