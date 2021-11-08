97167d5f3492d11d9af223af7b39b6bd18c860e0
Sunanda Gupta <51994186+sunandagupta@users.noreply.github.com>
2021-11-08 10:56:03 +0530

    Create Day7:Arrays.cpp

---
+++ 30 Days of Code/C++/Day7:Arrays.cpp
@@ -0,0 +1,63 @@
+/*
+Objective
+Today, we will learn about the Array data structure. Check out the Tutorial tab for learning materials and an instructional video.
+
+Task
+Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
+
+Example
+
+
+Print 4 3 2 1. Each integer is separated by one space.
+
+Input Format
+
+The first line contains an integer,  (the size of our array).
+The second line contains  space-separated integers that describe array 's elements.
+
+Constraints
+
+Constraints
+
+, where  is the  integer in the array.
+Output Format
+
+Print the elements of array  in reverse order as a single line of space-separated numbers.
+
+Sample Input
+
+4
+1 4 3 2
+Sample Output
+
+2 3 4 1
+
+*/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        int arr_item;
+        cin>>arr_item;
+        arr[i] = arr_item;
+    }
+    
+    for(int j = n-1; j>=0; j--){
+        cout<<arr[j]<<" ";
+    }
+
+    return 0;
+}
+
